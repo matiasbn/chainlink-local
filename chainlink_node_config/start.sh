2327eae58f4b292ce43049bd872569f03bf48c8f
Matías Barrios <matias.barriosn@gmail.com>
2020-12-11 19:34:36 -0300

    feat: chainlink node script created

---
+++ chainlink_node_config/start.sh
@@ -0,0 +1,1 @@
+docker run -p 6688:6688 -v ~/.chainlink-kovan:/chainlink -it --env-file=chainlink-env smartcontract/chainlink local n
--- migrations/2_chainlink_migration.js
+++ migrations/2_chainlink_migration.js
@@ -1,6 +1,7 @@
 const { LinkToken } = require("@chainlink/contracts/truffle/v0.4/LinkToken");
 const { Oracle } = require("@chainlink/contracts/truffle/v0.6/Oracle");
 
+// Oracle remix gist-> https://remix.ethereum.org/#gist=03a079b9055f42d993d0066d6f454c6f&optimize=true&version=soljson-v0.4.24+commit.e67f0147.js&runs=200&evmVersion=null
 module.exports = async (deployer, network, [defaultAccount]) => {
   LinkToken.setProvider(deployer.provider);
   Oracle.setProvider(deployer.provider);
